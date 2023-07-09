11b0f354daad16e496a4ccaefdde247daffc7e6f
chojaework <chojaework@gmail.com>
2023-07-09 19:55:05 +0900

    jaewon zzang

--- app/src/main/java/com/example/login/Frag3.java
+++ app/src/main/java/com/example/login/Frag3.java
@@ -1,7 +1,9 @@
 package com.example.login;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,18 +16,18 @@ public class Frag3 extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_frag3, container, false);
 
-        Button btnStretch1 = root.findViewById(R.id.btn_stretch_1);
-        btnStretch1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Replace Frag3 with Frag3_1
-                Fragment frag3_1 = new Frag3_1();
-                requireActivity().getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.nav_host_fragment, frag3_1)
-                        .addToBackStack(null)
-                        .commit();
-            }
-        });
+
+//        btnStretch1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // Replace Frag3 with Frag3_1
+//                Fragment frag3_1 = new Frag3_1();
+//                requireActivity().getSupportFragmentManager().beginTransaction()
+//                        .replace(R.id.nav_host_fragment, frag3_1)
+//                        .addToBackStack(null)
+//                        .commit();
+//            }
+//        });
 
         return root;
     }
@@ -35,4 +37,17 @@ public class Frag3 extends Fragment {
 //        // Clear the back stack when Frag3 is resumed
 //        requireActivity().getSupportFragmentManager().popBackStack();
 //    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+//        Button btnStretch1 = root.findViewById(R.id.btn_stretch_1);
+        view.findViewById(R.id.btn_stretch_1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Stretch1Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/login/Stretch1Activity.java
+++ app/src/main/java/com/example/login/Stretch1Activity.java
@@ -1,25 +1,24 @@
 package com.example.login;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 
-public class Frag3_1 extends Fragment {
+public class Stretch1Activity extends AppCompatActivity {
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_frag3_1, container, false);
-        // do some initialization here, if needed
-        YouTubePlayerView youTubePlayerView = root.findViewById(R.id.youtube_player_view);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stretch1);
+
+        YouTubePlayerView youTubePlayerView = findViewById(R.id.youtube_player_view);
         getLifecycle().addObserver(youTubePlayerView);
 
         youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
@@ -30,6 +29,13 @@ public class Frag3_1 extends Fragment {
                 youTubePlayer.loadVideo(videoId, 0);
             }
         });
-        return root;
+
+        Button btn_BackToFrag3 = findViewById(R.id.btn_BackToFrag3);
+        btn_BackToFrag3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
