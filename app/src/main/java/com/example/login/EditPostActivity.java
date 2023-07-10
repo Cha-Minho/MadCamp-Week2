47682245cf2bca9afe49634df3c99ae1dd0f3243
chojaework <chojaework@gmail.com>
2023-07-10 14:50:30 +0900

    중후반 commit

---
+++ app/src/main/java/com/example/login/EditPostActivity.java
@@ -0,0 +1,112 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class EditPostActivity extends AppCompatActivity {
+    private EditText titleEditText;
+    private EditText contentEditText;
+    private Button saveButton;
+
+    private String postTitle;
+    private String postContent;
+    private int postId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_post);
+
+        titleEditText = findViewById(R.id.edit_text_title_revise);
+        contentEditText = findViewById(R.id.edit_text_content_revise);
+        saveButton = findViewById(R.id.button_save_revise);
+
+        // Retrieve the initial title and content from the intent
+        Intent intent = getIntent();
+        postTitle = intent.getStringExtra("postTitle");
+        postContent = intent.getStringExtra("postContent");
+        postId = intent.getIntExtra("postId", -1);
+
+        // Set the initial title and content in the EditText fields
+        titleEditText.setText(postTitle);
+        contentEditText.setText(postContent);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get the updated title and content from the EditText fields
+                String updatedTitle = titleEditText.getText().toString();
+                String updatedContent = contentEditText.getText().toString();
+
+                // Call the method to send the POST request
+                sendPostToServer(updatedTitle, updatedContent);
+
+                // Send the updated title and content back to the calling activity
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra("updatedTitle", updatedTitle);
+                resultIntent.putExtra("updatedContent", updatedContent);
+                setResult(RESULT_OK, resultIntent);
+                finish();
+            }
+        });
+    }
+    private void sendPostToServer(String title, String content) {
+        OkHttpClient client = new OkHttpClient();
+
+        // Construct the JSON object
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("title", title);
+            jsonObject.put("content", content);
+            jsonObject.put("postId", postId);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        // Create a request body with the appropriate MediaType
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        RequestBody body = RequestBody.create(JSON, jsonObject.toString());
+
+        // Build the request
+        Request request = new Request.Builder()
+                .url("https://6ef2-192-249-19-234.ngrok-free.app/edit_post/") // replace with your actual URL and append the ID of the post
+                .post(body)
+                .build();
+
+        // Execute the request and retrieve the response
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    throw new IOException("Unexpected code " + response);
+                } else {
+                    // Do something with the response
+                }
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/example/login/Frag1.java
+++ app/src/main/java/com/example/login/Frag1.java
@@ -1,9 +1,10 @@
 package com.example.login;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,7 +12,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -29,17 +29,21 @@ import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-import retrofit2.http.GET;
 
 public class Frag1 extends Fragment {
-    private static final String URL = "https://your-api-url.com/posts/"; // replace with your server's URL
+    public static final int NEW_POST_REQUEST_CODE = 1;
+    private static final String URL = "https://6ef2-192-249-19-234.ngrok-free.app/load_post/"; // replace with your server's URL
     public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
 
     private RecyclerView recyclerView;
     private FloatingActionButton fabNewPost;
     private PostAdapter postAdapter;
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        loadPosts();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -55,13 +59,26 @@ public class Frag1 extends Fragment {
         postAdapter = new PostAdapter();
         recyclerView.setAdapter(postAdapter);
 
+        postAdapter.setOnItemClickListener(new PostAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(Frag1.Post post) {
+                // 클릭된 게시물의 상세 내용을 보여주는 액티비티로 전환하기 위한 Intent를 생성합니다.
+                Intent intent = new Intent(getActivity(), PostDetailActivity.class);
+                intent.putExtra("postTitle", post.getPostTitle());
+                intent.putExtra("postContent", post.getPostContent());
+                intent.putExtra("postId", post.getPostId());
+                startActivity(intent);
+            }
+        });
+
+
         // Set up the FloatingActionButton
         fabNewPost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Here, you can start a new activity to create a new post
                 Intent intent = new Intent(getActivity(), NewPostActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent, NEW_POST_REQUEST_CODE);
             }
         });
 
@@ -69,6 +86,15 @@ public class Frag1 extends Fragment {
 
         return view;
     }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == NEW_POST_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            loadPosts();
+        }
+    }
+
 
     public class Post {
         private int postId;
@@ -103,6 +129,10 @@ public class Frag1 extends Fragment {
         public void setPostContent(String postContent) {
             this.postContent = postContent;
         }
+
+        public int getPostId() {
+            return postId;
+        }
     }
 
     private void loadPosts() {
@@ -129,11 +159,11 @@ public class Frag1 extends Fragment {
                         for (int i = 0; i < postsJsonArray.length(); i++) {
                             JSONObject postJson = postsJsonArray.getJSONObject(i);
 
-                            int postId = postJson.getInt("PostID");
-                            String postTitle = postJson.getString("PostTitle");
-                            String postContent = postJson.getString("PostContext");
-                            int userId = postJson.getInt("UserID");
-                            String postDate = postJson.getString("PostDate");
+                            int postId = postJson.getInt("id");
+                            String postTitle = postJson.getString("title");
+                            String postContent = postJson.getString("context");
+                            int userId = postJson.getInt("user_id");
+                            String postDate = postJson.getString("date");
 
                             Post post = new Post(postId, postTitle, postContent, userId, postDate);
                             posts.add(post);
